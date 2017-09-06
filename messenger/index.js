192ddb09ad29c954c8e4c239b77828082ec2d6ef
Felix Volny <felix@volny.co>
2017-09-06 18:01:02 +0700

    refactor done

--- messenger/index.js
+++ messenger/index.js
@@ -1,39 +1,7 @@
-const { verifyToken } = require('./secrets')
-const { receivedMessage, receivedPostback } = require('./messaging')
+const request = require('request-promise-native')
+const { pageAccessToken, verifyToken } = require('./secrets')
+const { echo } = require('./messaging')
 
-/**
- * HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-const echo = (req, res) => {
-  const data = req.body
-  if (data.object === 'page') {
-    // fb might batch entries and send multiple
-    data.entry.forEach((entry) => {
-      const pageID = entry.id
-      const timeOfEvent = entry.time
-      console.log(`New Event: ${pageID} at ${timeOfEvent}`) // iterate over each messaging event
-      entry.messaging.forEach((event) => {
-        if (event.message) {
-          receivedMessage(event)
-        } else if (event.postback) {
-          receivedPostback(event)
-        } else {
-          console.log('Webhook received unknown event:', event)
-        }
-      })
-    })
-  }
-}
-
-/**
- * HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
 const verify = (req, res) => {
   if (req.query['hub.verify_token'] === verifyToken) {
     res.send(req.query['hub.challenge'])
@@ -42,11 +10,94 @@ const verify = (req, res) => {
   }
 }
 
+const callSendAPI = (messageData) => {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: pageAccessToken },
+    method: 'POST',
+    json: messageData,
+    resolveWithFullResponse: true
+  })
+    .then((response) => {
+      if (response.statusCode === 200) {
+        const recipientID = response.body.recipient_id
+        const messageID = response.body.message_id
+        console.log('Successfully sent message with id %s to recipient %s', messageID, recipientID)
+      }
+    })
+    .catch((error, response) => {
+      console.error('Unable to send message.')
+      console.error(response)
+      console.error(error)
+    })
+}
+
+const sendTextMessage = (recipientID, messageText) => {
+  const messageData = {
+    recipient: {
+      id: recipientID
+    },
+    message: {
+      text: messageText
+    }
+  }
+  callSendAPI(messageData)
+}
+
+const receivedMessage = (func, event) => {
+  const senderID = event.sender.id
+  const recipientID = event.recipient.id
+  const timeOfMessage = event.timestamp
+  const message = event.message
+
+  console.log('Received message for user %d and page %d at %d with message:', senderID, recipientID, timeOfMessage)
+  console.log(JSON.stringify(message))
+
+  const messageText = message.text
+  const messageAttachments = message.attachments
+
+  const messageToSend = func.apply(this, [event])
+  sendTextMessage(...messageToSend)
+
+  // start actual echo code
+  // if (messageText) {
+  //   switch (messageText) {
+  //     case 'generic':
+  //       sendGenericMessage(senderID)
+  //       break
+  //     default:
+  //       sendTextMessage(senderID, messageText)
+  //   }
+  // } else if (messageAttachments) {
+  //   sendTextMessage(senderID, 'Message with attachment received')
+  // }
+  // end actual echo code
+}
+
 const decorator = (func) => {
   const decorated = (req, res) => {
     const handlePOST = (req, res) => {
-      // code ...
-      func.apply(this, [req, res])
+      const data = req.body
+      if (data.object === 'page') {
+        // fb might batch entries and send multiple
+        data.entry.forEach((entry) => {
+          const pageID = entry.id
+          const timeOfEvent = entry.time
+          console.log(`New Event: ${pageID} at ${timeOfEvent}`) // iterate over each messaging event
+          entry.messaging.forEach((event) => {
+            if (event.message) {
+              receivedMessage(func, event)
+            }
+            // else if (event.postback) {
+            //   receivedPostback(event)
+            // }
+            else {
+              console.log('Webhook received unknown event:', event)
+            }
+          })
+        })
+      }
+
       // must respons with 200 or FB will resend request
       res.sendStatus(200)
     }
@@ -64,3 +115,4 @@ const decorator = (func) => {
 }
 
 exports.echo = decorator(echo)
+exports.verify = verify
--- messenger/messaging.js
+++ messenger/messaging.js
@@ -1,124 +1,87 @@
-const request = require('request-promise-native')
-const { pageAccessToken } = require('./secrets')
-
-const sendGenericMessage = (recipientID, messageText) => {
-  const messageData = {
-    recipient: {
-      id: recipientID
-    },
-    message: {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: [{
-            title: 'rift',
-            subtitle: 'Next-generation virtual reality',
-            item_url: 'https://www.oculus.com/en-us/rift/',
-            image_url: 'https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10460/10460569.jpg',
-            buttons: [{
-              type: 'web_url',
-              url: 'https://www.oculus.com/en-us/rift/',
-              title: 'Open Web URL'
-            }, {
-              type: 'postback',
-              title: 'Call Postback',
-              payload: 'Payload for first bubble'
-            }]
-          }, {
-            title: 'touch',
-            subtitle: 'Your Hands, Now in VR',
-            item_url: 'https://www.oculus.com/en-us/touch/',
-            image_url: 'https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10460/10460569.jpg',
-            buttons: [{
-              type: 'web_url',
-              url: 'https://www.oculus.com/en-us/touch/',
-              title: 'Open Web URL'
-            }, {
-              type: 'postback',
-              title: 'Call Postback',
-              payload: 'Payload for second bubble'
-            }]
-          }]
-        }
-      }
-    }
-  }
-
-  callSendAPI(messageData)
-}
-
-const sendTextMessage = (recipientID, messageText) => {
-  const messageData = {
-    recipient: {
-      id: recipientID
-    },
-    message: {
-      text: messageText
-    }
-  }
-  callSendAPI(messageData)
-}
-
-const callSendAPI = (messageData) => {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: pageAccessToken },
-    method: 'POST',
-    json: messageData,
-    resolveWithFullResponse: true
-  })
-    .then((response) => {
-      if (response.statusCode === 200) {
-        const recipientID = response.body.recipient_id
-        const messageID = response.body.message_id
-        console.log('Successfully sent generic message with id %s to recipient %s', messageID, recipientID)
-      }
-    })
-    .catch((error, response) => {
-      console.error('Unable to send message.')
-      console.error(response)
-      console.error(error)
-    })
-}
-
-const receivedMessage = (event) => {
+const echo = (event) => {
   const senderID = event.sender.id
-  const recipientID = event.recipient.id
-  const timeOfMessage = event.timestamp
   const message = event.message
-
-  console.log('Received message for user %d and page %d at %d with message:', senderID, recipientID, timeOfMessage)
-  console.log(JSON.stringify(message))
-
-  const messageID = message.mid
   const messageText = message.text
   const messageAttachments = message.attachments
 
   if (messageText) {
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID)
-        break
-      default:
-        sendTextMessage(senderID, messageText)
-    }
+    // switch (messageText) {
+    //   case 'generic':
+    //     sendGenericMessage(senderID)
+    //     break
+    //   default:
+        // echo the message
+
+        // sendTextMessage(senderID, messageText)
+
+        return [senderID, messageText]
+
+    // }
   } else if (messageAttachments) {
-    sendTextMessage(senderID, 'Message with attachment received')
+    // sendTextMessage(senderID, 'Message with attachment received')
+    return [senderID, 'Don\'t know what to do with message with attachment']
   }
 }
 
-const receivedPostback = (event) => {
-  const senderID = event.sender.id
-  const recipientID = event.recipient.id
-  const timeOfPostback = event.timestamp
+// const sendGenericMessage = (recipientID, messageText) => {
+//   const messageData = {
+//     recipient: {
+//       id: recipientID
+//     },
+//     message: {
+//       attachment: {
+//         type: 'template',
+//         payload: {
+//           template_type: 'generic',
+//           elements: [{
+//             title: 'rift',
+//             subtitle: 'Next-generation virtual reality',
+//             item_url: 'https://www.oculus.com/en-us/rift/',
+//             image_url: 'https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10460/10460569.jpg',
+//             buttons: [{
+//               type: 'web_url',
+//               url: 'https://www.oculus.com/en-us/rift/',
+//               title: 'Open Web URL'
+//             }, {
+//               type: 'postback',
+//               title: 'Call Postback',
+//               payload: 'Payload for first bubble'
+//             }]
+//           }, {
+//             title: 'touch',
+//             subtitle: 'Your Hands, Now in VR',
+//             item_url: 'https://www.oculus.com/en-us/touch/',
+//             image_url: 'https://multimedia.bbycastatic.ca/multimedia/products/500x500/104/10460/10460569.jpg',
+//             buttons: [{
+//               type: 'web_url',
+//               url: 'https://www.oculus.com/en-us/touch/',
+//               title: 'Open Web URL'
+//             }, {
+//               type: 'postback',
+//               title: 'Call Postback',
+//               payload: 'Payload for second bubble'
+//             }]
+//           }]
+//         }
+//       }
+//     }
+//   }
+//
+//   callSendAPI(messageData)
+// }
 
-  var payload = event.postback.payload
+// const receivedPostback = (event) => {
+//   const senderID = event.sender.id
+//   const recipientID = event.recipient.id
+//   const timeOfPostback = event.timestamp
+//
+//   var payload = event.postback.payload
+//
+//   console.log('Received postback for user %d and page %d with payload \'%s\'' + 'at %d', senderID, recipientID, payload, timeOfPostback)
+//
+//   sendTextMessage(senderID, 'Postback called')
+// }
 
-  console.log('Received postback for user %d and page %d with payload \'%s\'' + 'at %d', senderID, recipientID, payload, timeOfPostback)
-
-  sendTextMessage(senderID, 'Postback called')
-}
-
-module.exports.receivedMessage = receivedMessage
-module.exports.receivedPostback = receivedPostback
+// module.exports.receivedMessage = receivedMessage
+// module.exports.receivedPostback = receivedPostback
+module.exports.echo = echo
