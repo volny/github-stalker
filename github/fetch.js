8d2ef3f48d83c9b2ec1ea33047bae1dba7e3c191
Felix Volny <felix@volny.co>
2017-09-07 10:24:01 +0700

    can fetch avatar url from github

--- github/fetch.js
+++ github/fetch.js
@@ -1,10 +1,21 @@
 const request = require('request-promise-native')
 
-const getAvatar = (username) => {
+const getAvatar = (req, res) => {
+  const username = req.query.username
+  // res.status(200).send({username: username})
   const URI = `https://api.github.com/users/${username}`
-  const url = request(URI)
-    .then((data) => data.avatar_url)
-  return url
+  request({
+    uri: URI,
+    headers: {
+      'User-Agent': 'Github Stalker'
+    }
+  })
+    // .then(data => res.status(200).send({url: data.avatar_url}))
+    .then(data => res.status(200).send({url: JSON.parse(data).avatar_url}))
+    .catch((error) => {
+      console.log(error)
+      res.status(500).send({error: 'Something went wrong'})
+    })
 }
 
 module.exports.getAvatar = getAvatar
